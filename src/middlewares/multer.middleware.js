d142ec7bb46ae515e45636bf19fe8922d226cd27
ankit <ankit@bellurbis.com>
2024-07-05 16:26:47 +0530

    path updated in multer file

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,8 +1,16 @@
 const multer = require('multer')
+const fs = require('fs');
+const path = require('path');
+
+// Ensure the directory exists
+const uploadDir = path.join(__dirname, 'public', 'temp');
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "../assets")
+        cb(null, uploadDir)
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname)
