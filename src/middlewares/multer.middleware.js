cbe65031c0dc88bbf06d4977f92f187c5dc94f95
ankit <ankit@bellurbis.com>
2024-07-05 15:44:07 +0530

    path updated in multer file

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,7 +2,7 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "./public/temp")
+        cb(null, "./src/assets")
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname)
