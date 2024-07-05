258f0b6015617e959c551c9be0362a78ee746950
ankit <ankit@bellurbis.com>
2024-07-05 16:48:39 +0530

    path updated in multer file

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -5,7 +5,8 @@ const fs = require('fs');
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_NAME, 
   api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_SECRET 
+  api_secret: process.env.CLOUDINARY_SECRET ,
+  secure: true
 });
 
 const uploadOnCloudinary = async(localFilePath)=> {
