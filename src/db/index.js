daaec7ce7de4af8f4bb549ec2ad515bc4702da67
ankit <ankit@bellurbis.com>
2024-07-05 12:52:02 +0530

    fixing main file

--- src/db/index.js
+++ src/db/index.js
@@ -5,6 +5,7 @@ const connectDB = async()=>{
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`\n mongo db connected !! DB HOST : ${connectionInstance.connection.host}`);
+        return true;
     } catch (error) {
         console.log("mongo db connection error : ",error);
         process.exit(1);
